49c66dc05e232ccd72926ee46af3c2e71b4388f1
afif <afif.belhani@gmail.com>
2025-06-25 22:09:30 +0200

    New Blog Post on 2025-06-25 22:09:30

---
+++ public/searchindex.en.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "France 101",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "France 101",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "France 101",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "France 101",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
